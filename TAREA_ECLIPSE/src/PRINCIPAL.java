41312a9b63fecc07e8523b8ef56885ccc425c1f7
kevineitor22 <kevindavidzarate.daw@ciudadescolarfp.es>
2022-04-22 14:23:55 +0200

    Paso 8

--- TAREA_ECLIPSE/src/PRINCIPAL.java
+++ TAREA_ECLIPSE/src/PRINCIPAL.java
@@ -2,5 +2,7 @@
 public class PRINCIPAL {
 	public static void main(String[] args) {
 		System.out.println("listado de alumnos");
+		System.out.println("Alumno 1");
+		Alumno a = new Alumno("David", "Pérez", "873739923D");
 	}
 }
