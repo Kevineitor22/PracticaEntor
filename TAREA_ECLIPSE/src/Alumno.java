ed9d745c13ff19feea441492303699c199c9bd1f
kevineitor22 <kevindavidzarate.daw@ciudadescolarfp.es>
2022-04-22 14:21:05 +0200

    Paso 6

--- TAREA_ECLIPSE/src/Alumno.java
+++ TAREA_ECLIPSE/src/Alumno.java
@@ -2,4 +2,11 @@
 public class Alumno {
 	private String nombre, apellido, dni;
 
+	public Alumno(String nombre, String apellido, String dni) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+
 }
